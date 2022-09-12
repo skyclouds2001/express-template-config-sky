487b996f5a28b7bd3093841025388d208988e8d1
skyclouds2001 <skyclouds2001@163.com>
2022-09-12 20:25:03 +0800

    feat: add mysql support

---
+++ service/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: 'root123456',
+  database: 'csy',
+});
+
+module.exports = db;
