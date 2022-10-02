c515c363d4025db79277997a66d833bcce6012ad
skyclouds2001 <skyclouds2001@163.com>
2022-10-02 11:29:28 +0800

    feat: run lint

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session')
 
-const router = require('./routers/index');
+const router = require('./routers/index')
 
-const app = express();
+const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(cookieSession({
-  name: "session",
-  keys: ["%$#^&^%&TSFR#$TRGDRG$%GFDG%^$#%#^GFDGRDHG$#@^Y%"],
-  maxAge: 2 * 24 * 60 * 60 * 1000,
-}));
+  name: 'session',
+  keys: ['%$#^&^%&TSFR#$TRGDRG$%GFDG%^$#%#^GFDGRDHG$#@^Y%'],
+  maxAge: 2 * 24 * 60 * 60 * 1000
+}))
 
-app.use(router);
+app.use(router)
 
 app.listen(3000, () => {
-  console.log('App is running at http://localhost:3000');
-});
+  console.log('App is running at http://localhost:3000')
+})
