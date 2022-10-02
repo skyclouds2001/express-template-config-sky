e5cb928674c98bb43372b0a6aef1a1b47d8c424d
skyclouds2001 <skyclouds2001@163.com>
2022-10-02 11:16:36 +0800

    feat: config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require('eslint-define-config')
+
+module.exports = defineConfig({
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+    commonjs: true,
+    'shared-node-browser': true,
+    worker: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  globals: {},
+  plugins: [],
+  extends: [
+    'eslint:recommended',
+    'standard'
+  ],
+  rules: {},
+  settings: {},
+  overrides: []
+})
