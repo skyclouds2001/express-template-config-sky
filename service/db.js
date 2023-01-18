dadbaf5bb937193fc26ebfff22f6d933a486f92b
skyclouds2001 <skyclouds2001@163.com>
2023-01-18 11:56:58 +0800

    feat: hide db info

--- service/db.js
+++ service/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const db = mysql.createPool({
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: 'root123456',
-  database: 'csy',
+  host: '',
+  port: 0,
+  user: '',
+  password: '',
+  database: '',
 })
 
 module.exports = db
